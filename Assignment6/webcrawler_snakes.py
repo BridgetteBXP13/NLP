4a78597859c2db2c595c55088ceabcdf2066676e
BridgetteBXP13 <62217158+BridgetteBXP13@users.noreply.github.com>
2022-10-05 22:51:10 -0500

    updating crawler

--- Assignment6/webcrawler_snakes.py
+++ Assignment6/webcrawler_snakes.py
@@ -4,45 +4,65 @@
 from bs4 import BeautifulSoup
 import requests
 
-def crawl(starter_url):
-    r = requests.get(starter_url)
+def crawl(count, que, file):
+    print("In crawl")
+    print("count: ", count)
+    # If we have reached our maximum count, return
+    if count == 10:
+        return
+    if len(que) == 0:
+        return
 
+    r = requests.get(que.pop())
     data = r.text
     soup = BeautifulSoup(data, features="lxml")
 
-    counter = 0
     # write urls to a file
-    with open('snake_urls.txt', 'w') as f:
-        for link in soup.find_all('a'):
-            link_str = str(link.get('href'))
-            #print(link_str)
-            if 'Snake' in link_str or 'snake' in link_str:
-                if link_str.startswith('/url?q='):
-                    link_str = link_str[7:]
-                    #print('MOD:', link_str)
-                if '&' in link_str:
-                    i = link_str.find('&')
-                    link_str = link_str[:i]
-                if link_str.startswith('http') and 'google' not in link_str:
-                    f.write(link_str + '\n')
-                    crawl(link_str)
+
+    for link in soup.find_all('a'):
+        link_str = str(link.get('href'))
+        print(link_str)
+        if 'Snake' in link_str or 'snake' in link_str:
+            if link_str.startswith('/url?q='):
+                link_str = link_str[7:]
+                #print('MOD:', link_str)
+            if '&' in link_str:
+                i = link_str.find('&')
+                link_str = link_str[:i]
+            if link_str.startswith('http') and 'google' not in link_str and 'buzzfeed' not in link_str:
+                file.write(link_str + '\n')
+                que.append(link_str)
+                print("saved: ", link_str)
+                count += 1
+                # If we have reached our maximum count, return
+                if count == 10:
+                    return
+                print("count 2: ", count)
+                crawl(count, que, file)
+                print("count 3: ", count)
+                # If we have reached our maximum count, return
+                if count == 10:
+                    return
+    return
+
 
 if __name__== '__main__':
+
     starter_url = 'https://www.nationalgeographic.com/animals/reptiles/facts/snakes-1'
-
-    # Call the crawler for the starting url
-    crawl(starter_url)
-
     que = []
+    count = 0
+    que.append(starter_url)
 
-    # Still debugging below
+    with open('snake_urls.txt', 'w') as file:
+        file.write(starter_url + '\n')
+        crawl(count, que, file)
+
+
     with open('snake_urls.txt', 'r') as file:
         urls = file.read().splitlines()
     for u in urls:
-        #while len(que) <= 15:
-            #que.append(u)
         print(u)
-        crawl(u)
+
 
 
     # end of program
